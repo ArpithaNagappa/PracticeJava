f431c6c32e525e2e84b406b285fe0ad65f7ad7ce
Arpitha Nagappa <arpithagn021@gmail.com>
2024-07-18 15:23:05 -0500

    add

---
+++ src/main/java/org/example/Addition.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Addition {
+
+    int num1 =3;
+    int num2 = 4;
+
+    public static int addTwoNum(int num1,int num2){
+        return num1 + num2;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Addition add = new Addition();
+        int result = add.addTwoNum(4,3);
+        System.out.println(result);
+    }
+
+}
