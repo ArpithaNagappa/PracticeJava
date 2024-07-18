90a08bb62054a4563a51b399719a35026c914241
Arpitha Nagappa <arpithagn021@gmail.com>
2024-07-18 16:20:43 -0500

    new one

--- src/main/java/org/example/Addition.java
+++ src/main/java/org/example/Addition.java
@@ -2,10 +2,15 @@ package org.example;
 
 public class Addition {
 
-    int num1 =3;
-    int num2 = 4;
+    int num1;
+    int num2;
+    int num3;
 
     public static int addTwoNum(int num1,int num2){
         return num1 + num2;
+    }
+
+    public static int add3Numbers(int num1,int num2,int num3){
+        return num1 + num2+num3;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Addition add = new Addition();
-        int result = add.addTwoNum(4,3);
+        Addition obj = new Addition();
+        int result = obj.addTwoNum(4,3);
         System.out.println(result);
+
+        int sum = obj.add3Numbers(2,3,4);
+        System.out.println(sum);
+
+
     }
 
 }
